714fcf3508d2daf825030c777535195885384285
cnkmmk <22782790+cnkmmk@users.noreply.github.com>
2024-05-18 13:16:00 +0800

    Update update_hysteria.sh

--- update_hysteria.sh
+++ update_hysteria.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 systemctl stop hysteria-server
 arch=`dpkg --print-architecture`
+echo $arch
 hysver=`curl -Ls "https://api.github.com/repos/apernet/hysteria/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+echo $hysver
 curl -L -o /usr/local/bin/hysteria https://github.com/apernet/hysteria/releases/download/${hysver}/hysteria-linux-${arch}
 chmod +x /usr/local/bin/hysteria
 setcap cap_net_bind_service=+eip /usr/local/bin/hysteria
